9715612060e3ec91a92940c9e2c374614c9a2b0f
Pauli Jokela <pauli.jokela@didstopia.com>
2017-08-13 13:30:41 +0300

    Fixing versioning and CI issues

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,10 +5,18 @@ set -o pipefail
 
 BUILD_CONFIG=$1
 NUGET_API_KEY=$2
+NUGET_SOURCE_URL=${3:-https://www.nuget.org/api/v2/package}
 
 if [[ "$BUILD_CONFIG" == "Release" ]]; then
-	dotnet restore
-	dotnet build --configuration ${BUILD_CONFIG}
-	dotnet pack --configuration ${BUILD_CONFIG}
-	dotnet nuget push Didstopia.PDFSharp/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package
+	if [ -z ${var+x} ]; then
+		VERSION_SUFFIX="SNAPSHOT-$(git rev-list HEAD | wc -l)-$(git rev-parse --short HEAD)"
+	else
+		VERSION_SUFFIX=""
+	fi
+
+	dotnet restore /p:VersionSuffix=$VERSION_SUFFIX
+	dotnet build --configuration ${BUILD_CONFIG} /p:VersionSuffix=$VERSION_SUFFIX
+	dotnet pack --configuration ${BUILD_CONFIG} --no-build --version-suffix $VERSION_SUFFIX
+
+	dotnet nuget push Didstopia.PDFSharp/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE_URL
 fi
